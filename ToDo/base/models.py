eb43c9cdc170837b3deed8dee90d9961ef106d63
rajiv <unickrajiv3@gmail.com>
2023-10-30 21:03:07 +0545

    inital commit

---
+++ ToDo/base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+# Create your models here.
+class ToDo(models.Model):
+    status_list=[('Done','Done'),('Not done','Not done'),('In progress','In progress')]  # this is for the status because status only has limited input(list of choices)
+    # and the choices are written in status_list variable twice is because one is for read and one is for write
+    name = models.CharField(max_length=200) #this charfield only allows database to store string .
+    description =models.TextField() #same thing as charfild but very high length
+    status =models.CharField(max_length=50,choices=status_list) #satuts_list is passed to choices
+
+
+
+
+ 
